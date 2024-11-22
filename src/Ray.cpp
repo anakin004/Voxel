521e9e76c42532bdb9db8b496345781f16a5eef0
anakin004 <160974989+anakin004@users.noreply.github.com>
2024-11-21 17:29:32 -0800

    raytracing

---
+++ src/Ray.cpp
@@ -0,0 +1,16 @@
+#include "Ray.h"
+#include <iostream>
+
+Ray::Ray(glm::vec3 start, glm::vec3 ori) : m_Pos(start), m_Ori(ori)
+{
+}
+
+glm::vec3& Ray::march()
+{
+	const float step = 0.5;
+	m_Pos.x += step * m_Ori.x;
+	m_Pos.y += step * m_Ori.y;
+	std::cout << m_Pos.y << std::endl;
+	m_Pos.z += step * m_Ori.z;
+	return m_Pos;
+}
