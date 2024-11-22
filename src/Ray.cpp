4ed80d2d6b8de104f1e17c6cc189bd9eadf5fd16
anakin004 <160974989+anakin004@users.noreply.github.com>
2024-11-21 20:34:18 -0500

    removed debug printing

--- src/Ray.cpp
+++ src/Ray.cpp
@@ -10,7 +10,6 @@ glm::vec3& Ray::march()
 	const float step = 0.5;
 	m_Pos.x += step * m_Ori.x;
 	m_Pos.y += step * m_Ori.y;
-	std::cout << m_Pos.y << std::endl;
 	m_Pos.z += step * m_Ori.z;
 	return m_Pos;
 }
