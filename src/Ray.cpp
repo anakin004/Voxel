d0b6ec2adb2dc4f2bffee2030a0a2e74a57cac27
anakin004 <160974989+anakin004@users.noreply.github.com>
2024-12-05 01:45:36 -0500

    Update Ray.cpp

--- src/Ray.cpp
+++ src/Ray.cpp
@@ -7,7 +7,7 @@ Ray::Ray(glm::vec3 start, glm::vec3 ori) : m_Pos(start), m_Ori(ori)
 
 glm::vec3& Ray::march()
 {
-	// step can be changed - the smaller the better
+	// step can be changed for efficiency
 	const float step = 0.25;
 	m_Pos.x += step * m_Ori.x;
 	m_Pos.y += step * m_Ori.y;
